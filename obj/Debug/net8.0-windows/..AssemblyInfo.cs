373db27da1bb92ac76bb55eaa45794d1f3247305
ChiuTien <chiumandresychristian@gmail.com>
2026-03-24 17:52:59 +0300

    dfsd

--- obj/Debug/net8.0-windows/..AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/..AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2161940c88e90f0e4a6061325de1aec73f4b6b00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f91ea858457385b5998693394c9a582fc0438859")]
 [assembly: System.Reflection.AssemblyProductAttribute(".")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
