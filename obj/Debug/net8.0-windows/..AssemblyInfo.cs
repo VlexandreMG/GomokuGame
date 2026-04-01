139eac58194afed0a40b294f9062e0b5384db811
VlexandreMG <dodolashmou@gmail.com>
2026-04-01 12:38:05 +0200

    le mety ito

--- obj/Debug/net8.0-windows/..AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/..AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46d2d9ebb42587d62f4f9215c925d0a0d3deb2ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2139b446d5a21d89ad5c70195d4bd60561edf68")]
 [assembly: System.Reflection.AssemblyProductAttribute(".")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
